b307334769df0140f44324762f659c5318d1b3a3
Kevin Read <kevin.read@qr.com.au>
2009-04-01 14:08:14 +1000

    Fixed blank space check for has_registry_value on win32 platform

--- lib/sprinkle/verifiers/registry.rb
+++ lib/sprinkle/verifiers/registry.rb
@@ -27,7 +27,14 @@ module Sprinkle
       def has_registry_value(key, name, value)
         key, name, value = key.to_s, name.to_s, value.to_s
         if RUBY_PLATFORM =~ /win32/
-          command = "reg query \"#{key}\" /v \"#{name}\" | findstr /c:\"#{value}\""
+          if value.empty?
+            # The regular expression includes a [\t ], which is a tab character and a space.
+            # XP delimits the output with 1 tab, Win2003 trails with 4 spaces!
+            # Note: Checking for blank and a value of space(s) will report a false positive.
+            command = "reg query \"#{key}\" /v \"#{name}\" | findstr /r /c:\"^.*#{Regexp.quote name}.*REG_[A-Z]*[\t ]*$"
+          else
+            command = "reg query \"#{key}\" /v \"#{name}\" | findstr /r /c:\"^.*#{Regexp.quote name}.*REG_[A-Z]*[\t ]*#{Regexp.quote value}$"
+          end
           command << ' > NUL 2>&1' unless logger.debug?
         else
           raise NotImplementedError, "Non-win32 platforms do not support checking for registry settings"
